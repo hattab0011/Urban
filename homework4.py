dde676485a6ec3a8259ca4829d2de8c3c761a3fc
hattab0011 <Hattab0011@yandex.ru>
2024-07-08 19:41:56 +0300

    Create homework4.py

---
+++ homework4.py
@@ -0,0 +1,14 @@
+# Создайте переменную my_string и присвойте ей значение строки с произвольным текстом (функция input()).
+# Вывести количество символов введённого текста
+# Выведите строку my_string в верхнем регистре.
+# Выведите строку my_string в нижнем регистре.
+# Измените строку my_string, удалив все пробелы.
+# Выведите первый символ строки my_string.
+# Выведите последний символ строки my_string.
+my_string = input()
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ",''))
+print(my_string[0])
+print(my_string[-1])
