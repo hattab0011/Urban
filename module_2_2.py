a45a443e28071d04b35f8a1e7a5c08e55bd122b3
hattab0011 <Hattab0011@yandex.ru>
2024-07-11 13:41:55 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,6 +1,4 @@
-first = input()
-second = input()
-third = input()
+first, second, third  = input(), input(), input()
 if first == second == third:
     print('3')
 elif first == second and first != third or first != second and first == third:
