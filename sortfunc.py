fecbc21c51fcf7d6a1cb7ef7a27d7090f397eb7c
hattab0011 <Hattab0011@yandex.ru>
2024-08-01 20:52:23 +0300

    Create sortfunc.py

---
+++ sortfunc.py
@@ -0,0 +1,26 @@
+nums = [5, 6, 2, 1, 3, 4]
+
+def bubble_sort(ls):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(ls) - 1):
+            if ls[i] > ls[i+1]:
+                ls[i], ls[i+1] = ls[i+1], ls[i]
+                swapped = True
+
+
+bubble_sort(nums)
+print(nums)
+
+def selection_sort(ls):
+    for i in range(len(ls)):
+        lowest = i
+        for j in range(i + 1, len(ls)):
+            if ls[j] < ls[lowest]:
+                lowest = j
+        ls[i], ls[lowest] = ls[lowest], ls[i]
+
+
+selection_sort(nums)
+print(nums)
