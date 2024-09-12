996799900b517006e98c620a786e1e5207784776
hattab0011 <Hattab0011@yandex.ru>
2024-09-12 20:22:47 +0300

    Create module_9_3.py

---
+++ module_9_3.py
@@ -0,0 +1,10 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+
+first_result = (abs(len(first) - len(second)) for first, second in zip(first, second) if len(first) != len(second))
+second_result = (abs(len(first[i]) - len(second[i])) for i in range(min(len(first), len(second))))
+
+
+print(list(first_result))
+print(list(second_result))
