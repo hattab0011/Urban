5c4f9cab299d823033911634208bc70f3f1e03cd
hattab0011 <Hattab0011@yandex.ru>
2024-09-16 18:06:19 +0300

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -3,7 +3,7 @@ second = ['Строка', 'Урбан', 'Компьютер']
 
 
 first_result = (abs(len(first) - len(second)) for first, second in zip(first, second) if len(first) != len(second))
-second_result = (abs(len(first[i]) - len(second[i])) for i in range(min(len(first), len(second))))
+second_result = (abs(len(first[i]) == len(second[i])) for i in range(min(len(first), len(second))))
 
 
 print(list(first_result))
