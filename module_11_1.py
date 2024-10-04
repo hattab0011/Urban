7072e7f692cf546b63aa0562cde626de15dae114
hattab0011 <Hattab0011@yandex.ru>
2024-10-04 15:54:56 +0300

    Update module_11_1.py

--- module_11_1.py
+++ module_11_1.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import json
 
-# Функция для обработки JSON и добавления данных в DataFrame
 def process_json_file(input_file, output_file):
     formatted_data = []
 
-    # Чтение и обработка большого JSON файла
     with open(input_file, 'r', encoding='utf-8') as file:
         for line in file:
-            entry = json.loads(line)  # Если файл в формате "один JSON объект на одну строку"
+            entry = json.loads(line)
             formatted_entry = {
                 "name": entry.get("name", ""),
                 "company": entry.get("company", ""),
@@ -17,13 +15,10 @@ def process_json_file(input_file, output_file):
             }
             formatted_data.append(formatted_entry)
 
-    # Создание DataFrame
     df = pd.DataFrame(formatted_data)
 
-    # Сохранение в Excel
     df.to_excel(output_file, index=False, engine='openpyxl')
 
     print(f"Данные успешно сохранены в файл: {output_file}")
 
-# Пример вызова функции
 process_json_file('1-50.json', 'output.xlsx')
