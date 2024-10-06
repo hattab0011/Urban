26b48c9c0a9531f116d0abb79dbe9014515c8300
hattab0011 <Hattab0011@yandex.ru>
2024-10-06 19:24:33 +0300

    Update module_11_2.py

--- module_11_2.py
+++ module_11_2.py
@@ -5,6 +5,7 @@ def introspection_info(obj):
     info = {
         'type': type(obj),
         'Атрибуты обьекта': dir(obj),
+        'Методы обьекта': [attr for attr in dir(obj) if callable(getattr(obj, attr))],
         'Модуль, к которому объект принадлежит': getmodule(obj)
     }
     return info
