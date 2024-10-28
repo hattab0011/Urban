c503b5a75d51616551b462c4eb03cafc247fbe0a
hattab0011 <Hattab0011@yandex.ru>
2024-10-28 15:03:05 +0300

    module_14_1

---
+++ module_14_1.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER,
+    balance INTEGER NOT NULL            
+)
+''')
+
+cursor.execute(" CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
+users_data = []
+for i in range(10):
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES(?, ?, ?, ?)', (f"User{i+1}",f"example{i+1}@gmail.com",f"{i+1}*10",f"1000"))
+    users_data.append(i)
+
+for j in range(1, len(users_data), 2):
+    cursor.execute('UPDATE Users SET balance = balance - 500 WHERE id = ?', (j + 1,))
+
+for k in range(0, len(users_data), 3):
+    cursor.execute('DELETE FROM Users WHERE id = ?', (k + 1,))
+
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
+results = cursor.fetchall()
+
+for user in results:
+    print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
+
+connection.commit()
+connection.close()
