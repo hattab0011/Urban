9b9d91716e8a0916ac21aa3b7de26f2f599b7c27
hattab0011 <Hattab0011@yandex.ru>
2025-01-09 18:09:08 +0300

    Update module_16_1.py

--- FAST API/module_16_1.py
+++ FAST API/module_16_1.py
@@ -7,15 +7,13 @@ async def root():
     return {"massage": "Главная страница"}
 
 @app.get("/user/admin")
-def admin_page():
+async def admin_page():
     return {"message": "Вы вошли как администратор"}
 
 @app.get("/user/{user_id}")
-def user_page(user_id: int):
+async def user_page(user_id: int):
     return {"message": f"Вы вошли как пользователь № {user_id}"}
 
 @app.get("/user")
-def user_info(username: [str] = None, age: [int] = None):
-    if username and age:
+async def user_info(username: [str] = None, age: [int] = None):
         return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
-    return {"message": "Пожалуйста, укажите имя и возраст пользователя"
